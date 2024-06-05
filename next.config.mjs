4c435264c9d87372987e46a689f8fb49581afd77
michal-bulas <michal.bulas1@gmail.com>
2024-06-05 19:54:11 +0200

    Change images extensions due to nextjs error

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,16 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      { hostname: 'localhost', pathname: '**', port: '3000', protocol: 'http' },
+      {
+        hostname: 'localhost',
+        pathname: '**',
+        port: '3000',
+        protocol: 'http'
+      },
       {
         hostname: 'ui-hippo-production.up.railway.app',
         pathname: '**',
+        port: '',
         protocol: 'https'
       }
     ]
