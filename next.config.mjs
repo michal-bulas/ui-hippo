4d353857b5ed6394e690448b717c7fc18a75624c
michal-bulas <michal.bulas1@gmail.com>
2024-06-05 19:02:50 +0200

    Add production hostname to nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { hostname: 'localhost', pathname: '**', port: '3000', protocol: 'http' }
+      { hostname: 'localhost', pathname: '**', port: '3000', protocol: 'http' },
+      {
+        hostname: 'ui-hippo-production.up.railway.app',
+        pathname: '**',
+        protocol: 'https'
+      }
     ]
   }
 };
